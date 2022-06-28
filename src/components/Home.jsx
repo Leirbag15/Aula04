da82ad0e969027f74525149e3084552fd9cd5c55
Leirbag15 <gabriel.bezerra1501@gmail.com>
2022-06-28 13:08:28 -0300

    ativd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import FirebaseUserService from "../services/FirebaseUserService";
 import FirebaseContext from "../utils/FirebaseContext";
 
--- src/services/FirebaseUserService.js
+++ src/services/FirebaseUserService.js
@@ -1,7 +1,38 @@
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth'
+import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 
 export default class FirebaseUserService {
-
+    static authE = (auth,login) => {
+        const actionCodeSettings = {
+            // URL you want to redirect back to. The domain (www.example.com) for this
+            // URL must be in the authorized domains list in the Firebase Console.
+            url: 'https://www.example.com/finishSignUp?cartId=1234',
+            // This must be true.
+            handleCodeInApp: true,
+            iOS: {
+              bundleId: 'com.example.ios'
+            },
+            android: {
+              packageName: 'com.example.android',
+              installApp: true,
+              minimumVersion: '12'
+            },
+            dynamicLinkDomain: 'example.page.link'
+          };
+        sendSignInLinkToEmail(auth, login, actionCodeSettings)
+      .then(() => {
+        // The link was successfully sent. Inform the user.
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', login);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+      });
+    }
     static signup = (auth, login, password, callback) => {
         createUserWithEmailAndPassword(
             auth,
